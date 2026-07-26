0c21c06223b277b3729f13ae3e7c6d3643237e28
midkids <83714759+midkids@users.noreply.github.com>
2026-07-26 11:54:25 -0400

    Checking for a valid address

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Myron Snelson on 7/25/26.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
